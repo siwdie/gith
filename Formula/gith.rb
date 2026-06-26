e9e8331a5eba0426fc610f8b02c777232307e5d7
siwdie <codyjmc@gmail.com>
2026-06-26 12:53:40 +0200

    feat: update brew formula to v1.7.0

--- Formula/gith.rb
+++ Formula/gith.rb
@@ -1,18 +1,18 @@
 class Gith < Formula
   desc "Configurable git workflow CLI with branch creation, rebase, commit, and squash helpers"
   homepage "https://github.com/siwdie/gith"
-  version "1.6.0"
+  version "1.7.0"
   license "MIT"
 
   on_arm do
-    url "https://github.com/siwdie/gith/releases/download/v1.6.0/gith-v1.6.0-macos-arm64"
-    sha256 "aa3c2301e16736114b2dd2e9926ad76d9527130c167d0d00c540f9015cf03b0d"
+    url "https://github.com/siwdie/gith/releases/download/v1.7.0/gith-v1.7.0-macos-arm64"
+    sha256 "65a50a72597c02ff7ca5aae8f7e53adb64882f2f4f7630f306a7fe392097cf3a"
   end
 
   def install
     odie "gith is currently available only for Apple Silicon (arm64)." unless Hardware::CPU.arm?
 
-    bin.install "gith-v1.6.0-macos-arm64" => "gith"
+    bin.install "gith-v1.7.0-macos-arm64" => "gith"
   end
 
   test do
